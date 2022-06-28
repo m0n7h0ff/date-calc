4098abbc84e5e09eb3c7975dbe4c122a87ab4067
m0n7h0ff <al.monah.off@gmail.com>
2022-06-28 18:49:32 +0500

    1я рабочая версия

--- date_calc/employee.go
+++ date_calc/employee.go
@@ -3,6 +3,15 @@ package main
 import "time"
 
 type Employee struct {
-	FIO string
-	start time.Time
+	FIO   string
+	start *time.Time
+}
+
+func (e *Employee) NewEmployee(name string, year, month, day int) {
+	e.FIO = name
+	e.start = e.setDate(year, month, day)
+}
+func (e *Employee) setDate(year, month, day int) *time.Time {
+	date := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+	return &date
 }
--- date_calc/main.go
+++ date_calc/main.go
@@ -11,69 +11,79 @@ import (
 )
 
 func main() {
+	emp1 := new(Employee)
+	emp1.NewEmployee("Монахов", 2022, 6, 3)
 
-		/* e2 := Employee{
-		FIO: "Перехода",
-		start: Date(2022,6,7),
+	emp2 := new(Employee)
+	emp2.NewEmployee("Дубинин", 2022, 6, 5)
+
+	emp3 := new(Employee)
+	emp3.NewEmployee("Тюшняков", 2022, 6, 1)
+
+	emp4 := new(Employee)
+	emp4.NewEmployee("Перехода", 2022, 6, 7)
+
+	mapOfEmployee := make(map[string]Employee)
+
+	mapOfEmployee[emp1.FIO] = *emp1
+	mapOfEmployee[emp2.FIO] = *emp2
+	mapOfEmployee[emp3.FIO] = *emp3
+	mapOfEmployee[emp4.FIO] = *emp4
+
+	enteredName := readInput("Введите фамилию:\t")
+
+	foundEmployee, ok := mapOfEmployee[enteredName]
+	if !ok {
+		log.Fatal("Имени нет")
 	}
+	startDate := *foundEmployee.start
+	enteredDate := readInput("Введите дату (формат 2022-06-30):\t")
 
-	e3 := Employee{
-		FIO: "Тюшняков",
-		start: Date(2022,6,1),
-	}
-
-	e4 := Employee{
-		FIO: "Дубинин",
-		start: Date(2022,6,5),
-	}
-
-	fmt.Println(e1,e2,e3,e4) */
-
-	e1 := Employee{
-		FIO: "Монахов",
-		start: Date(2022,6,3),
-	}
-
-	tNow := e1.start
-	days := Subs(tNow)
-	_, ost := math.Modf(float64(days) / 8.0)
-	fmt.Printf("Раздница в днях: %v, остаток %v", days, ost)
-	fmt.Println()
-
-	printResult(ost)
-}
-func Date(year, month, day int) time.Time {
-	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
-}
-
-func Subs(startDate time.Time) int {
-	
-
-	input := readStr("Введите число:")
-
-	grade, err := time.Parse("2006-01-02", input)
+	parseDate, err := time.Parse("2006-01-02", enteredDate)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Неправильный формат даты: %v \n", err)
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+        fmt.Println(input)
 	}
 
-	t1 := Date(startDate.Year(), int(startDate.Month()), startDate.Day())
-	t2 := Date(grade.Year(), int(grade.Month()), grade.Day())
+	differenceBetweenDates := getDifferenceBetweenDates(startDate, parseDate)
+	_, fractional := math.Modf(float64(differenceBetweenDates) / 8.0) //остаток
+	//fmt.Printf("Раздница в днях: %v, остаток %v", differenceBetweenDates, fractional)
+	fmt.Println()
+	printResult(fractional)
 
-	days := t2.Sub(t1).Hours() / 24
+
+	//2 строки что-бы консоль не закрывалась сразу
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	fmt.Println(input)
+}
+
+//func Date(year, month, day int) *time.Time {
+//	date := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+//	return &date
+//}
+
+func getDifferenceBetweenDates(startDate time.Time, enteredDate time.Time) int {
+	days := enteredDate.Sub(startDate).Hours() / 24
 	return int(days)
 }
 
-func readStr(s string) string{
-	fmt.Print(s)
+func readInput(message string) string {
+	fmt.Print(message)
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
+		fmt.Println("Ошибка при считывании")
+		input, _ := reader.ReadString('\n')
+		fmt.Println(input)
 		log.Fatal(err)
 	}
 	return strings.TrimSpace(input)
 }
 
-func printResult(ost float64){
+func printResult(ost float64) {
 	switch ost {
 	case 0:
 		fmt.Println("1-я смена, в день")
@@ -91,4 +101,3 @@ func printResult(ost float64){
 		fmt.Println("Fail/////")
 	}
 }
-
